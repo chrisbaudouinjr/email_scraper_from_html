9091edb4d843b95a69d5cfdf13c10b699dfb64da
Jon <jonathan@classnavapp.com>
2018-05-17 11:14:20 -0500

    First commit

---
+++ emails.py
@@ -0,0 +1,21 @@
+import re
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+
+email_regex = r'[\w.-]+@[\w.-]+'
+
+for roots, dirs, files in os.walk('./html'):
+    for name in files:
+        if not name.startswith('.'):
+            filepath = os.path.join(roots, name)
+            content = open(filepath)
+            soup = BeautifulSoup(content, "lxml")
+            text = soup.get_text()
+            emails = re.findall(email_regex,text)
+            df = pd.DataFrame(data=emails)
+            filename = name.strip('.html') + '_emails.csv'
+            df.to_csv(os.path.join('./output',filename),index=False,header=False)
+            os.rename(filepath,os.path.join('./processed_html', name))
+
+
